77d13bb4c2b204122294565c5f6db91ad590138a
Ayman Abusura <aymansalameh1996@hotmail.com>
2024-11-27 16:07:28 +0300

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Navbar, Hero } from "@/sections/index"
+import { Navbar, Hero, LogoTicker } from "@/sections/index"
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <Hero />
+            <LogoTicker />
         </>
     );
 }
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,25 @@
 import logoImage from './images/logo.svg';
 import designExample1Image from './images/design-example-1.png';
 import designExample2Image from './images/design-example-2.png';
+import quantumLogo from "./images/quantum.svg";
+import acmeLogo from "./images/acme-corp.svg";
+import echoValleyLogo from "./images/echo-valley.svg";
+import pulseLogo from "./images/pulse.svg";
+import outsideLogo from "./images/outside.svg";
+import apexLogo from "./images/apex.svg";
+import celestialLogo from "./images/celestial.svg";
+import twiceLogo from "./images/twice.svg";
 
 export {
     logoImage,
     designExample1Image,
-    designExample2Image
+    designExample2Image,
+    quantumLogo,
+    acmeLogo,
+    echoValleyLogo,
+    pulseLogo,
+    outsideLogo,
+    apexLogo,
+    celestialLogo,
+    twiceLogo
 };
